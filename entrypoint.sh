db00caf7af68971097f0d89398f7a6791071099e
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-08-06 22:22:06 -0400

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -l
 
+ls -alh
+echo "running syft..."
 syft packages "docker:$1" -o json=sbom.syft.json -o spdx-json=sbom.spdx.json
+echo "...Done."
+ls -alh
 cat sbom.syft.json
 
+echo "running grype..."
 grype "docker:$1" -o json > grype.json
+echo "...Done."
+ls -alh
 cat grype.json
 
 findings=$(cat grype.json)
