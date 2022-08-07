e869d9f509a2856f70d318b5ffdda42a7158f06e
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-08-06 22:32:55 -0400

    no sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 ls -alh
 echo "running syft..."
-sudo syft packages "docker:$1" -o json=sbom.syft.json -o spdx-json=sbom.spdx.json
+syft packages "docker:$1" -o json=sbom.syft.json -o spdx-json=sbom.spdx.json
 echo "...Done."
 ls -alh
 cat sbom.syft.json
 
 echo "running grype..."
-sudo grype "docker:$1" -o json > grype.json
+grype "docker:$1" -o json > grype.json
 echo "...Done."
 ls -alh
 cat grype.json
