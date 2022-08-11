77698ad5878c9b536e2cf352162aa9440b8a75f2
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-08-10 22:33:23 -0400

    test fail-on

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,16 @@ echo "...Done."
 head sbom.syft.json
 
 echo "running grype..."
-grype "docker:$1" -o json > grype.json
+grype "docker:$1" -o json --fail-on "$2" > grype.json
 echo "...Done."
 head grype.json
 
 zip findings.zip sbom.syft.json sbom.spdx.json grype.json
 
 ls -alh
+
+## TODO Scan the grype, compare with input threshold, emit success or failure
+
 
 # Set an output?
 # findings=$(cat grype.json)
